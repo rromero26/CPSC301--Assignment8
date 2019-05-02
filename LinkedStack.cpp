c59585ce357e2670950fb99a04182b5ce3f2d258
Ryan <rromero26@csu.fullerton.edu>
2019-05-01 19:06:22 -0700

    update

--- LinkedStack.cpp
+++ LinkedStack.cpp
@@ -3,57 +3,81 @@
 
 template<class Type>
 LinkedStack<Type>::LinkedStack() : head(nullptr) {
-    
-} // end default constructor
 
+}
+//--------------------------------------------------------------
 template<class Type>
 LinkedStack<Type>::LinkedStack(const LinkedStack<Type> & aStack) {
     // TODO: Implement pseudocode
     Node<Type> * originalPointer = nullptr;
     // Point to nodes in original chain
-    
+
     if(originalPointer == nullptr) {
-    
+
     }
     else {
         // Copy first node
-        
-        // Point to first node in new chain 
-        
+
+        // Point to first node in new chain
+
         // Copy remaining nodes
         while(originalPointer != nullptr) {
-            // Advance original-chain pointer 
+            // Advance original-chain pointer
             // Get next item from original chain
-            // Create a new node containing the next item 
-            // Link new node to end of new chain 
-            // Advance pointer to new last node 
-        } // end while 
+            // Create a new node containing the next item
+            // Link new node to end of new chain
+            // Advance pointer to new last node
+        } // end while
         // Set the end of new chain
-    } // end if 
-} // end copy constructor
-
+    } // end if
+}
+//--------------------------------------------------------------
 template<class Type>
 LinkedStack<Type>::~LinkedStack() {
-    // TODO: pop until empty
-} // end destructor
-
+  Node<Type> * tempHead = nullptr;          //create temp ptr
+    while(isEmpty() != true){               //while stack is not empty, delete nodes
+      tempHead = head;
+      head = head->next;
+      delete tempHead;
+    }
+}
+//--------------------------------------------------------------
 template<class Type>
 bool LinkedStack<Type>::isEmpty() {
-    // TODO: stub function provided for demo, implement logic
-    return true;
-} // end isEmpty
-
+    if(head != nullptr)                     // if element exist, return false
+      {return false;}
+    else                                    // if head = NULL, return true
+    {return true;}
+}
+//--------------------------------------------------------------
 template<class Type>
 bool LinkedStack<Type>::push(Type & newElement) {
-    // TODO: implement function
-} // end push
+    Node<TYPE> *temp = new Node;                  //creates new node
+    temp->data = newElement;                      //sets element of node
 
+    temp->next = head;                            // add it to front of "stack"
+    head = temp;                                  //has head point to top
+}
+//--------------------------------------------------------------
 template<class Type>
 bool LinkedStack<Type>::pop() {
-    // TODO: implement function
-} // end pop
+
+    Node<TYPE> *temp = new Node;
+    temp = head;
+    TYPE holder = head->data;
+    head = head->next;                             //move head to next element
+
+    delete temp;                                   //delete previous elemnt
+    return holder;                                 //return value of deleted element
+}
+//--------------------------------------------------------------
 
 template<class Type>
 Type LinkedStack<Type>::peek() {
-    // TODO: implement function - handle empty case first
-} // end peek
+  if(isEmpty() == true){                           //check if stack is empty
+    cout << "ERROR: stack is empty" << endl;
+    return 0;
+  }
+
+  return head->data;                               //return value of top
+}
